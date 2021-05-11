3cf7b4d43a53cbd40f5bdc16dba67d6b990bb58e
jahadul-rakib <rakibdiu2015@gmail.com>
2021-05-11 11:08:46 +0600

    add

--- src/variables.go
+++ src/variables.go
@@ -6,16 +6,27 @@ import (
 )
 
 func main() {
-	fmt.Println("From Package.")
 
 	var name = "Rakib"
+
 	age := 30
+
 	var phone string = "0180000111203"
+
 	const COUNTRY = "Bangladesh"
+
 	const INDIA string = "India"
 
 	println("name: ", name, " Age: ", age, " Phone: ", phone)
 	println("Name Type", reflect.TypeOf(name))
 	println("Age Type: ", reflect.TypeOf(age))
 	println("Phone Type: ", reflect.TypeOf(phone))
+
+	var preDeclear string
+	var preDeclear1 string
+
+	preDeclear = "preDeclear word"
+	preDeclear1 = "preDeclear1 word"
+
+	fmt.Println(preDeclear, preDeclear1)
 }
