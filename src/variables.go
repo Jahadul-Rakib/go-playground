a7636bf4c466b36507cc8fb4de2c3a7ab8398da1
jahadul-rakib <rakibdiu2015@gmail.com>
2021-05-11 11:00:18 +0600

    add

--- src/variables.go
+++ src/variables.go
@@ -1,16 +1,21 @@
 package main
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
+	fmt.Println("From Package.")
 
-	var name = "rakib"
+	var name = "Rakib"
 	age := 30
-	var phone string = "rakib"
+	var phone string = "0180000111203"
+	const COUNTRY = "Bangladesh"
+	const INDIA string = "India"
 
 	println("name: ", name, " Age: ", age, " Phone: ", phone)
 	println("Name Type", reflect.TypeOf(name))
 	println("Age Type: ", reflect.TypeOf(age))
 	println("Phone Type: ", reflect.TypeOf(phone))
-
 }
